d7ea02d831c99348d7fb0645e61c951fb570d97b
kenshinpg <kenshinpg@hotmail.com>
2019-09-09 10:17:16 +0800

    requirement.txt

--- train/device.py
+++ train/device.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 import torch
-import pynvml
 
 def check_cuda(configs):
 
@@ -13,12 +12,3 @@ def check_cuda(configs):
 		device = torch.device("cpu")
 		use_gpu = False
 	return device, use_gpu
-
-def gpu_memory_occupancy(configs):
-
-	total_memory = configs['gpu_memory']
-	pynvml.nvmlInit()
-	# 这里的0是GPU id
-	handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-	meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
-	return '%.3fMiB//%dMiB' %(float(meminfo.used/1024/1024), total_memory)
